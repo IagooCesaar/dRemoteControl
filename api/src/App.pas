69cac7ecc49e6d9518e5994be9fad949369b5c2a
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2023-01-20 06:48:16 -0300

    Iniciando serviço horse

--- api/src/App.pas
+++ api/src/App.pas
@@ -7,14 +7,26 @@ procedure Start;
 implementation
 
 uses
-  StrUtils, DateUtils, SysUtils, Math;
+  System.SyncObjs,
+  System.SysUtils,
+  Horse;
 
 procedure Start;
-var LPort : Integer;
+var LPort : Integer; LContext : string;
 begin
-
+  // O número da porta pode ser passada por parâmetro para o executável
   LPort :=  StrToIntDef(ParamStr(1), 9000);
 
+
+
+  THorse.Listen(LPort,
+    procedure(Horse: THorse)
+    begin
+      Writeln(Format('Server is runing on %s:%d', [Horse.Host, Horse.Port]));
+      Writeln(Format('Try use Swagger on http://localhost:%d%s/swagger-ui',
+        [Horse.Port, LContext]));
+      ReadLn;
+    end);
 end;
 
 end.
